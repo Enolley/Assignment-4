b9bf195b0d08a3f6fc0a15f8e2cd64684678b332
enolley <enolleyomukamani@gmail.com>
2023-10-18 11:24:04 +0300

    Second Commit

--- part_one.py
+++ part_one.py
@@ -1,17 +1,17 @@
-# Get marks for four subjects: math, physics, geography, and chemistry
+
 math_marks = float(input("Enter math marks: "))
 physics_marks = float(input("Enter physics marks: "))
 geo_marks = float(input("Enter geography marks: "))
 chem_marks = float(input("Enter chemistry marks: "))
 
-# Check for negative values or values greater than 100
+
 if any(mark < 0 or mark > 100 for mark in [math_marks, physics_marks, geo_marks, chem_marks]):
     print("Unrecognized marks/average (Negative values or values greater than 100)")
 else:
-    # Calculate the average
+
     average = (math_marks + physics_marks + geo_marks + chem_marks) / 4
 
-    # Assign a grade based on the average
+
     if average >= 71 and average <= 100:
         print("Grade: A")
     elif average >= 51:
